4acf654accf7a198089a506dceab6aea876a7a57
DinandDeGamer <DinandSmeets@outlook.com>
2022-02-20 10:32:49 +0100

    i

---
+++ commands/mauw.js
@@ -0,0 +1,12 @@
+module.exports.run = async (client, message, args) => {
+
+    return message.channel.send("mauw mauw mauw: https://tenor.com/view/mau-puss-mau-ayo-cat-meow-gif-16247070");
+
+}
+
+module.exports.help = {
+
+    name: "mauw",
+    category: "general"
+
+}
---
+++ commands/rickroll.js
@@ -0,0 +1,12 @@
+module.exports.run = async (client, message, args) => {
+
+    return message.channel.send("Kijk heel goed naar de gift: https://tenor.com/view/challenge-find-out-when-this-gif-ends-rickroll-rickrolled-hidden-rickroll-gif-22493495");
+
+}
+
+module.exports.help = {
+
+    name: "rickroll",
+    category: "general"
+
+}
